1043b8979c7256f624e8dc0774678cc44fc32ae2
mcorcias <mosescorcias@gmail.com>
2021-02-12 22:08:06 +0200

    added callable function

--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,12 @@ requestModal.addEventListener('click', e => {
     requestModal.classList.remove('open');
   }
 });
+
+// say hello function call
+const button = document.querySelector('.call');
+button.addEventListener('click', async () => {
+  // get function reference
+  const sayHello = firebase.functions().httpsCallable('sayHello');
+  const res = await sayHello({ name: 'moshe' });
+  console.log(res.data);
+});
